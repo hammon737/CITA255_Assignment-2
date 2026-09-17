576bb191ddc785ca86dcb1b621c1d686af267cde
hammon737 <hammon737@morrisville.edu>
2026-09-17 17:17:22 -0400

    Add project files.

---
+++ CITA255_Assignment 2/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace CITA255_Assignment_2
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> Food = new List<string>
+        {
+            "",
+            "banana",
+            "lemon",
+            "lime",
+            "grapes",
+            "cherry",
+            "blueberry",
+            "blackberry",
+            "strawberry",
+            "rasberry"
+        };
+        List<double> prices = new List<double>
+        {
+            5,
+            20,
+            12,
+            16,
+            8,
+            17,
+            30,
+            6,
+            2,
+            19
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            myFoodList.ItemsSource = Food;
+            myPriceList.ItemsSource = prices;
+        }
+
+        private void testButtonClicked(object sender, EventArgs e)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total = total + price;
+                Debug.WriteLine(price);
+            }
+
+            sum.Text = total.ToString();
+
+
+        }
+    }
+}
